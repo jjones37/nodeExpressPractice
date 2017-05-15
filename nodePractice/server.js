6fd2477301a64df7211d28bc3ebb4cb9e51437a5
Joshua Jones <Joshua Jones>
2017-05-14 23:30:02 -0400

    Added Global Navbar

--- nodePractice/server.js
+++ nodePractice/server.js
@@ -3,18 +3,14 @@ var app = express()
 
 const path = require('path');
 
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-app.get('/', function (req, res) {
-  res.send('Donnie sucks')
-  console.log('Base app.get is working')
-})
-
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
---
+++ nodePractice/src/app/ui/ui.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { NavbarComponent } from './navbar.component';
+
+@NgModule({
+  declarations: [
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  exports: [NavbarComponent]
+})
+export class uiModule { }
