684ea7d65ea06546d68a5e33cca3fed08a79c592
Joshua Jones <Joshua Jones>
2017-05-14 23:05:05 -0400

    Initial commit2

--- nodePractice/server.js
+++ nodePractice/server.js
@@ -1,10 +1,20 @@
 var express = require('express')
 var app = express()
 
+const path = require('path');
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
 app.get('/', function (req, res) {
-  res.send('Donnie sucks cock from hell')
+  res.send('Donnie sucks')
   console.log('Base app.get is working')
 })
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
